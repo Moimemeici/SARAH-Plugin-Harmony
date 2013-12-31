592518915e37e56804ab69e1a84f3504f78645cc
Olivier Bagot <obagot@gmail.com>
2013-12-31 15:29:23 +0100

    First import.

---
+++ plugins/harmony/harmony.js
@@ -0,0 +1,131 @@
+/*
+ * Logitech Harmony plugin for S.A.R.A.H.
+ * https://github.com/hobbe/SARAH-Plugin-Harmony
+ *
+ * Licensed under DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *
+ * This plugin relies on Manuel Alabor's harmonyjs library for node.js
+ * https://github.com/swissmanu/harmonyjs
+ *
+ * You may use node debug module; set environment variable DEBUG=*
+ */
+
+exports.action = function(data, callback, config, SARAH){
+
+	// Fetch plugin configuration
+	config = config.modules.harmony;
+
+	// Check plugin configuration
+	if (!config.login || !config.ip){
+		callback({ 'tts': 'Vous devez configurer le plugin Harmony avec votre identifiant et mot de passe Logitek' });
+		return;
+	}
+
+	// Hub object for ease of use
+	var hub = {
+		"login": config.login,
+		"password": config.password,
+		"ip": config.ip
+	};
+
+	console.log('Harmony login: ' + config.login + ' on ip: ' + config.ip);
+
+	// Check SARAH action
+	if (data.action == "status") {
+		debug('action: status');
+		getStatus(hub, callback);
+	} else if (data.action == "list") {
+		debug('action: list');
+		getActivities(hub, callback);
+	} else if (data.action == "startactivity") {
+		debug('action: startactivity');
+		if (data.activity) {
+			debug('activity: ' + data.activity);
+			startActivity(data.activity, hub, callback);
+		} else {
+			callback({ 'tts': "Activité non renseignée" });
+		}
+	} else if (data.action == "turnoff") {
+		debug('action: turnoff');
+		turnOff(hub, callback);
+	} else {
+		callback({ 'tts': "Je n'ai rien fait" });
+	}
+};
+
+var debug = require('debug')('plugin:harmony');
+
+/**
+ * Get Harmony hub status.
+ */
+var getStatus = function(hub, callback) {
+	debug('getStatus()');
+	console.log('Checking Harmony status...');
+
+	var client = require('./harmonylib');
+
+	client.checkState(hub, function() {
+		console.log('Hub is off');
+		callback({ 'tts': "Harmonie est éteinte" });
+	}, function() {
+		console.log('Hub is on');
+		callback({ 'tts': "Harmonie est allumée" });
+	}, function(error) {
+		console.log('error: ' + error);
+		callback({ 'tts': "erreur" });
+	});
+};
+
+/**
+ * Get list of Harmony activities.
+ */
+var getActivities = function(hub, callback) {
+	debug('getActivities()');
+	console.log('Getting Harmony activities...');
+
+	var client = require('./harmonylib');
+
+	client.listActivities(hub, function(activities) {
+		console.log('List of Harmony activities');
+		callback({ 'tts': "Liste des activités non implémentée" });
+	}, function(error) {
+		console.log('error: ' + error);
+		callback({ 'tts': "erreur" });
+	});
+};
+
+/**
+ * Start Harmony activity given by activity name.
+ */
+var startActivity = function(activity, hub, callback) {
+	debug('startActivity(' + activity + ')');
+	console.log('Starting Harmony activity ' + activity + '...');
+
+	var client = require('./harmonylib');
+
+	client.startActivity(hub, activity, function() {
+		console.log('Harmony activity started');
+		callback({ 'tts': "Démarrage de l'activité " + activity });
+	}, function(error) {
+		console.log('error: ' + error);
+		callback({ 'tts': "erreur" });
+	});
+};
+
+/**
+ * Turn off current activity (Power off).
+ */
+var turnOff = function(hub, callback) {
+	debug('turnOff()');
+	console.log('Turning off Harmony...');
+
+	var client = require('./harmonylib');
+
+	client.turnOff(hub, function() {
+		console.log('Harmony turned off');
+		callback({ 'tts': "Arrêt de Harmony" });
+	}, function(error) {
+		console.log('error: ' + error);
+		callback({ 'tts': "erreur" });
+	});
+};
---
+++ plugins/harmony/harmonylib.js
@@ -0,0 +1,161 @@
+/*
+ * Logitech Harmony plugin for S.A.R.A.H.
+ * https://github.com/hobbe/SARAH-Plugin-Harmony
+ *
+ * Licensed under DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *
+ * This plugin relies on Manuel Alabor's harmonyjs library for node.js
+ * https://github.com/swissmanu/harmonyjs
+ *
+ * You may use node debug module; set environment variable DEBUG=*
+ */
+
+var debug = require('debug')('plugin:harmony:harmonylib');
+var harmony = require('harmonyjs');
+
+/**
+ * Check Harmony state.
+ * @param hub hub object { login, password, hub_ip }
+ * @param ifOff callback if hub is off
+ * @param ifOn callback if hub is on
+ * @param onError callback on error
+ */
+function checkState(hub, ifOff, ifOn, onError) {
+	debug('checkState()');
+	var promise = harmony(hub.login, hub.password, hub.ip);
+
+	promise.then(function(client) {
+		debug('successful client creation');
+		client.isOff().then(function(off) {
+			debug('successful client isOff()');
+
+			client.end();
+			if(off) {
+				if (ifOff) {
+					ifOff();
+				}
+			} else {
+				if (ifOn) {
+					ifOn();
+				}
+			}
+		}, function(error) {
+			debug('error on client isOff()');
+			client.end();
+			if (onError) {
+				onError(error);
+			}
+		});
+	}, function(error) {
+		debug('error on client creation');
+		if (onError) {
+			onError(error);
+		}
+	});
+}
+
+/**
+ * List actvities.
+ * @param hub hub object { login, password, hub_ip }
+ * @param onSuccess callback on success
+ * @param onFailure callback on failure
+ */
+function listActivities(hub, onSuccess, onFailure) {
+	debug('listActivities()');
+	var promise = harmony(hub.login, hub.password, hub.ip);
+
+	promise.then(function(client) {
+		debug('successful client creation');
+
+		// TODO: to be implemented
+
+		client.end();
+		if (onSuccess) {
+			onSuccess();
+		}
+	}, function(error) {
+		if (onError) {
+			onError(error);
+		}
+	});
+}
+
+/**
+ * Start an activity in the Harmony remote.
+ * @param hub hub object { login, password, hub_ip }
+ * @param activityName name of activity to start
+ * @param onSuccess callback on success
+ * @param onFailure callback on failure
+ */
+function startActivity(hub, activityName, onSuccess, onError) {
+	debug('startActivity()');
+	var promise = harmony(hub.login, hub.password, hub.ip);
+
+	promise.then(function(client) {
+		debug('successful client creation');
+
+		client.getActivities().then(function(activities) {
+			activities.some(function(activity) {
+				if (activity.label === activityName) {
+					var id = activity.id;
+					debug('activity ID for ' + activityName + ': ' + id);
+					console.log('Starting activity ' + activityName);
+					client.startActivity(id);
+					client.end();
+
+					if (onSuccess) {
+						onSuccess();
+					}
+					return true;
+				}
+				return false;
+			});
+		}, function(error) {
+			debug('error on client getActivities()');
+			client.end();
+			if (onError) {
+				onError(error);
+			}
+		});
+	}, function(error) {
+		debug('error on client creation');
+		if (onError) {
+			onError(error);
+		}
+	});
+}
+
+/**
+ * Turn off current activity on remote.
+ * @param hub hub object { login, password, hub_ip }
+ * @param onSuccess callback on success
+ * @param onFailure callback on failure
+ */
+function turnOff(hub, onSuccess, onError) {
+	debug('turnOff()');
+	var promise = harmony(hub.login, hub.password, hub.ip);
+
+	promise.then(function(client) {
+		debug('successful client creation');
+		console.log('Turning Harmony off');
+		client.turnOff();
+		client.end();
+		if (onSuccess) {
+			onSuccess();
+		}
+	}, function(error) {
+		debug('error on client creation');
+		if (onError) {
+			onError(error);
+		}
+	});
+}
+
+/*
+ * Exports.
+ */
+
+exports.checkState = checkState;
+exports.listActivities = listActivities;
+exports.startActivity = startActivity;
+exports.turnOff = turnOff;
